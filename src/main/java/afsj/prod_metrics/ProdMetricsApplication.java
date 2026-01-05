fac71620d601d031c3d753d7c9788c116cf9655e
AntonioJr <afs.junior21@gmail.com>
2026-01-05 11:08:10 -0300

    first commit

---
+++ src/main/java/afsj/prod_metrics/ProdMetricsApplication.java
@@ -0,0 +1,13 @@
+package afsj.prod_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdMetricsApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(ProdMetricsApplication.class, args);
+   }
+
+}
---
+++ src/test/java/afsj/prod_metrics/ProdMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package afsj.prod_metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdMetricsApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
