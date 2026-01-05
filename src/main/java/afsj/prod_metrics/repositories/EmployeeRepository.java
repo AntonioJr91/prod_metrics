cfc92b865243428fc34643471071650895143403
AntonioJr <afs.junior21@gmail.com>
2026-01-05 15:23:08 -0300

    fix: add missing @Repository annotation

--- src/main/java/afsj/prod_metrics/repositories/EmployeeRepository.java
+++ src/main/java/afsj/prod_metrics/repositories/EmployeeRepository.java
@@ -2,8 +2,10 @@ package afsj.prod_metrics.repositories;
 
 import afsj.prod_metrics.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
 }
